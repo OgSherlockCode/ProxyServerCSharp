547b9b55bea16af8743ff08b8a8e907650931e8a
OgSherlockCode <42701785+OgSherlockCode@users.noreply.github.com>
2020-11-30 14:07:39 +0100

    Socks5 not working

---
+++ ConsoleApp1/ConsoleApp1/NetworkComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace EasyNetworking
+{
+    public class NetworkComponent
+    {
+        public event EventHandler<Socket> Connected;
+        public void StartServer(string ip = "127.0.0.1", int port = 5000, ProtocolType protocolType = ProtocolType.Tcp)
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, protocolType);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                Console.WriteLine("Waiting for a connection...");
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+                    new Thread(() => { Connected?.Invoke(this, handler); }).Start();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+
+
+        public int chunkSize = 1024;
+        public byte[] Recive(Socket socket)
+        {
+            byte[] buffer = new byte[chunkSize];
+            int size = chunkSize;
+            List<byte> endBuffer = new List<byte>();
+            while (size == chunkSize)
+            {
+                size = socket.Receive(buffer, 0, chunkSize, SocketFlags.None);
+                if (size != chunkSize)
+                    Array.Resize(ref buffer, size);
+                endBuffer.AddRange(buffer);
+            }
+            return endBuffer.ToArray();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,96 @@
+﻿using EasyNetworking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProxyServer
+{
+    class Program
+    {
+        static NetworkComponent server = new NetworkComponent();
+        static void Main(string[] args)
+        {
+            server.Connected += new EventHandler<Socket>(Connect);
+            server.StartServer("127.0.0.1", 1080, ProtocolType.Tcp);
+        }
+
+        private static void Connect(object sender, Socket e)
+        {
+            try
+            {
+                byte[] buf = server.Recive(e);
+                int port = BitConverter.ToInt16(new byte[] { buf[3], buf[2] }, 0);
+                IPAddress ip = IPAddress.Parse(String.Format("{0}.{1}.{2}.{3}", buf[4], buf[5], buf[6], buf[7]));
+                Socket webServer = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                switch (buf[0])
+                {
+                    case 0x04:
+                        //Connecting client is SOCKS4
+                        Console.Write("Socks4|");
+                        switch (buf[1])
+                        {
+
+                            case 0x01:
+                                //Connect to ip
+                                webServer.Connect(ip, port);
+                                Console.WriteLine("Connecting|" + ip + ":" + port);
+                                e.Send(new byte[] { 0x00, 0x5A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+                                new Thread(() => { ExchangeData(webServer, e); }).Start();
+                                ExchangeData(e, webServer);
+                                break;
+
+                            case 0x02:
+                                //Bind ip port
+                                Console.WriteLine("Binding|" + ip + ":" + port);
+                                webServer.Bind(new IPEndPoint(ip, port));
+                                e.Send(new byte[] { 0x00, 0x5A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+                                new Thread(() => { ExchangeData(webServer, e); }).Start();
+                                ExchangeData(e, webServer);
+                                break;
+
+                            default:
+                                return;
+                        }
+                        break;
+
+                    case 0x05:
+                        //Connecting client is SOCKS5
+                        Console.Write("Socks5|");
+
+                        break;
+
+                    default:
+                        return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+        static void ExchangeData(Socket from,Socket to)
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buf = server.Recive(from);
+                    if (buf.Length == 0) break;
+                    to.Send(buf);
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+    }
+}
